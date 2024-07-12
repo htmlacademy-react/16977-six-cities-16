be83d018d7d69c686fc67dbe778dd25a92dd8f7d
IvanMain <ivan.main.developer@gmail.com>
2024-07-12 21:20:03 +0300

    Поправит ошибки EsLint

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,5 +1,5 @@
-const CITY_NAMES: string[] = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'] as const;
+const CITY_NAMES = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'] as const;
 
-const countOffersMainPage: number = 5 as const;
+const countOffersMainPage = 5 as const;
 
 export { CITY_NAMES, countOffersMainPage };
